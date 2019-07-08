5f881625c967c60d47e56ada096f52abb535c256
wangbin <wangbin04@wifi.com>
2019-07-08 17:45:01 +0800

    Enable DataBinding

--- app/src/main/java/tv/rings/data/ProductRelease.kt
+++ app/src/main/java/tv/rings/data/ProductRelease.kt
@@ -3,4 +3,4 @@ package tv.rings.data
 /**
  * Created by wangbin on 2019-07-08.
  */
-class ProductRelease(val country: String, val date: String, val version: String)
+data class ProductRelease(val country: String, val date: String, val version: String)
